7bd7df632df1a2fb66ea4962b982bbf2968787be
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-03-20 23:30:49 +0300

    Delete stats-service directory

--- stats-service/stats-server/src/main/java/ru/practicum/explorewithme/repository/StatsRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.explorewithme.repository;
-
-import java.util.List;
-import ru.practicum.explorewithme.HitStatDto;
-
-public interface StatsRepositoryCustom {
-
-  List<HitStatDto> getStats(String start, String end, List<String> uris, Boolean unique);
-}
