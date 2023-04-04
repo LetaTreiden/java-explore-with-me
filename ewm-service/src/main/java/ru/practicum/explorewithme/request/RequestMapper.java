ee1a8abf0b353f096f1eb551aa06c6ba8dce633f
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-04-05 02:13:24 +0300

    Add files via upload
    find all variables

--- ewm-service/src/main/java/ru/practicum/explorewithme/request/RequestMapper.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/request/RequestMapper.java
@@ -13,7 +13,7 @@ public class RequestMapper {
         .id(request.getId())
         .requesterId(request.getUser().getId())
         .eventId(request.getEvent().getId())
-        .created(request.getCreatedOn())
+        .created(request.getCreated())
         .status(request.getStatus())
         .build();
   }
