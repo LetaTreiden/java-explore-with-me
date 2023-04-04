7d112de3367b7521c18a0c0cd4d529cbcf0c6a79
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-04-05 02:23:21 +0300

    Request dto
    user to requester

--- ewm-service/src/main/java/ru/practicum/explorewithme/request/RequestMapper.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/request/RequestMapper.java
@@ -11,7 +11,7 @@ public class RequestMapper {
   public static RequestDto toDto(Request request) {
     return RequestDto.builder()
         .id(request.getId())
-        .requesterId(request.getUser().getId())
+        .requester(request.getUser().getId())
         .event(request.getEvent().getId())
         .created(request.getCreatedOn())
         .status(request.getStatus())
