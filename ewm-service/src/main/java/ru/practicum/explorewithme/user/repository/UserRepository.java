580bf94fb85142d73bbc42c4af4e12955ed2caf1
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-04-06 23:14:13 +0300

    Add files via upload

--- ewm-service/src/main/java/ru/practicum/explorewithme/user/repository/UserRepository.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/user/repository/UserRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.explorewithme.user.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.practicum.explorewithme.user.dto.UserDto;
 import ru.practicum.explorewithme.user.model.User;
 
 import java.util.List;
--- ewm-service/src/main/java/ru/practicum/explorewithme/user/service/UserServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/user/service/UserServiceImpl.java
@@ -1,12 +1,5 @@
 package ru.practicum.explorewithme.user.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import javax.transaction.Transactional;
-
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -15,10 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import ru.practicum.explorewithme.user.dto.UserDto;
 import ru.practicum.explorewithme.user.UserMapper;
+import ru.practicum.explorewithme.user.dto.UserDto;
 import ru.practicum.explorewithme.user.model.User;
 import ru.practicum.explorewithme.user.repository.UserRepository;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -27,25 +27,25 @@ import ru.practicum.explorewithme.user.repository.UserRepository;
 @Slf4j
 public class UserServiceImpl implements UserService {
 
-  private final UserRepository uRepo;
+    private final UserRepository uRepo;
 
-  @Override
-  @Transactional
-  public UserDto create(UserDto userDto) {
-    User user = uRepo.save(UserMapper.toUser(userDto));
-    return UserMapper.toUserDto(user);
-  }
+    @Override
+    @Transactional
+    public UserDto create(UserDto userDto) {
+        User user = uRepo.save(UserMapper.toUser(userDto));
+        return UserMapper.toUserDto(user);
+    }
 
-  @Override
-  public List<UserDto> getAll(List<Long> id, int from, int size) {
-    //Pageable pageable = PageRequest.of(from / size, size);
-    Pageable pageable = PageRequest.of(from, size);
-   List<User> users = uRepo.findAllByIdIn(id, pageable);
-   List<UserDto> userDtos = new ArrayList<>();
-   for (User u : users) {
-     userDtos.add(UserMapper.toUserDto(u));
-   }
-   return userDtos;
+    @Override
+    public List<UserDto> getAll(List<Long> id, int from, int size) {
+        //Pageable pageable = PageRequest.of(from / size, size);
+        Pageable pageable = PageRequest.of(from, size);
+        List<User> users = uRepo.findAllByIdIn(id, pageable);
+        List<UserDto> userDtos = new ArrayList<>();
+        for (User u : users) {
+            userDtos.add(UserMapper.toUserDto(u));
+        }
+        return userDtos;
     /*log.info("Возврат списка пользователей");
     if (id.isEmpty()) {
       return uRepo.findAll().stream()
@@ -58,33 +58,34 @@ public class UserServiceImpl implements UserService {
     }
 
      */
-  }
-
-  @Override
-  public List<UserDto> getAllTwo(List<Long> id, int from, int size) {
-    Pageable pageable = PageRequest.of(from, size);
-    log.info(pageable.toString());
-    return UserMapper.mapToUserDto(uRepo.findAllByIdIn(id, pageable));
-  }
-  @Override
-  public List<UserDto> findAllByIdIn(List<Long> ids, Integer from, Integer size) {
-
-    Pageable pageable = PageRequest.of(from, size);
-
-    return uRepo.findAllByIdIn(ids, pageable).stream()
-            .map(UserMapper::toUserDto)
-            .collect(Collectors.toList());
-  }
-
-  @Override
-  @Transactional
-  public void delete(long userId) throws NoSuchElementException {
-    Optional<User> user = uRepo.findById(userId);
-    if (user.isEmpty()) {
-      throw new NoSuchElementException("User not found");
     }
-    User user1 = uRepo.getReferenceById(userId);
-    log.info(user1.toString());
-    uRepo.deleteById(user1.getId());
-  }
+
+    @Override
+    public List<UserDto> getAllTwo(List<Long> id, int from, int size) {
+        Pageable pageable = PageRequest.of(from, size);
+        log.info(pageable.toString());
+        return UserMapper.mapToUserDto(uRepo.findAllByIdIn(id, pageable));
+    }
+
+    @Override
+    public List<UserDto> findAllByIdIn(List<Long> ids, Integer from, Integer size) {
+
+        Pageable pageable = PageRequest.of(from, size);
+
+        return uRepo.findAllByIdIn(ids, pageable).stream()
+                .map(UserMapper::toUserDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public void delete(long userId) throws NoSuchElementException {
+        Optional<User> user = uRepo.findById(userId);
+        if (user.isEmpty()) {
+            throw new NoSuchElementException("User not found");
+        }
+        User user1 = uRepo.getReferenceById(userId);
+        log.info(user1.toString());
+        uRepo.deleteById(user1.getId());
+    }
 }
