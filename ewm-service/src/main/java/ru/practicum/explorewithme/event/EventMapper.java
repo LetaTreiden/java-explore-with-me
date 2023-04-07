6f5cd903daec59b5f155aa6255416c965a30727e
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-04-08 02:07:33 +0300

    Add files via upload

--- ewm-service/src/main/java/ru/practicum/explorewithme/event/EventMapper.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/event/EventMapper.java
@@ -1,6 +1,8 @@
 package ru.practicum.explorewithme.event;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -75,7 +77,7 @@ public class EventMapper {
             : event.getDescription())
         .eventDate(inputEventDto.getEventDate() != null ? inputEventDto.getEventDate() : event.getEventDate())
         .paid(inputEventDto.getPaid() != null ? inputEventDto.getPaid() : event.getPaid())
-        .participantLimit(inputEventDto.getParticipantLimit() != null
+        .participantLimit(!Objects.equals(inputEventDto.getParticipantLimit(), null)
             ? inputEventDto.getParticipantLimit()
             : event.getParticipantLimit())
         .requestModeration(inputEventDto.getRequestModeration() != null
--- ewm-service/src/main/java/ru/practicum/explorewithme/event/dto/InputEventDto.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/event/dto/InputEventDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 
 import lombok.*;
 import ru.practicum.explorewithme.event.model.State;
@@ -23,18 +24,31 @@ public class InputEventDto {
 
   private String description;
 
+  @NotNull
   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
   private LocalDateTime eventDate;
 
+  @NotNull
   private Location location;
-  private Boolean paid;
-  private Integer participantLimit;
-  private Boolean requestModeration;
+  private boolean paid;
+
+  @PositiveOrZero
+  private int participantLimit;
+
+  private boolean requestModeration;
 
   @NotBlank
   private String title;
   private State state;
   private State stateAction;
+
+  public Boolean getPaid() {
+    return paid;
+  }
+
+  public Boolean getRequestModeration() {
+    return requestModeration;
+  }
 
   @Data
   @Getter
