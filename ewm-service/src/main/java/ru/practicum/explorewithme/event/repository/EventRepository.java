54b73d38c9001cdaa54a11b40129a4b877066530
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-04-10 03:22:11 +0300

    Add files via upload

--- ewm-service/src/main/java/ru/practicum/explorewithme/event/repository/EventRepository.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/event/repository/EventRepository.java
@@ -3,10 +3,7 @@ package ru.practicum.explorewithme.event.repository;
 import java.util.List;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.practicum.explorewithme.event.dto.EventInfo;
 import ru.practicum.explorewithme.event.model.Event;
-import ru.practicum.explorewithme.event.model.EventStatus;
 
 public interface EventRepository extends JpaRepository<Event, Long>, EventRepositoryCustom {
   List<Event> findAllByInitiatorId(long initiatorId, PageRequest pageRequest);
